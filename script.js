24bee81757f6de5482ef193c77fb706114c900fa
jstivez <jstivez@gmail.com>
2021-12-07 19:55:32 -0500

    added new features

--- script.js
+++ script.js
@@ -1,14 +1,46 @@
-const char = 'abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()-' 
-var prom = window.prompt('enter password length');
-let result = ''; {
-for (let i = 1; i <= prom; i++ ){
-    const rand = Math.floor(Math.random() * 46 +1);
-    let b = (char.substring(rand, rand+1));   
-    result += b;
-}
-function myFunc() { 
-document.getElementById('password').innerHTML = result;
-}
-}
+const numb = '1234567890'
+const char = 'abcdefghijklmnopqrstuvwxyz' 
+const cap = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const spec= '~!@#$%^&*()_+'
 
- 
+let numCheckVar = window.confirm ('numbers?');
+let charVar =  window.confirm ('lowercase');
+let capCharVar = window.confirm ('upper case');
+let specCharVar=  window.confirm ('special characters')
+let numberLength = prompt ('length of password')
+
+let confirmLength = numberLength;
+
+let pwChars = '';
+let randResults= '';
+
+/*while(confirmLength <= 7 || confirmLength>= 121) {
+    alert("password must be between 8 - 120");
+    var confirmLength =(prompt ('length of password'));
+    }*/
+
+if (numCheckVar) {
+    pwChars = pwChars.concat(numb);
+}
+if (charVar) {
+    pwChars = pwChars.concat(char);
+}
+if (specCharVar) {
+    pwChars = pwChars.concat(spec);
+}
+if (capCharVar) {
+    pwChars = pwChars.concat(cap);
+}
+console.log(pwChars);
+function gen (){
+for (let i = 1; i <= numberLength; i++ ){
+    const rand = Math.floor(Math.random() * pwChars.length);
+    let b = (pwChars.substring(rand, rand+1));   
+    randResults += b;
+}
+}
+gen();
+function myFunc() { 
+        document.getElementById('password').innerHTML = randResults;
+
+}
